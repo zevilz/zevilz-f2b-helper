b61d0c7a4e1c288aa4fc6adeb6c218a4d9ed7c58
zEvilz <zevilz13@yandex.ru>
2022-04-13 02:15:55 +0500

    Added

---
+++ zevilz-f2b-helper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+Plugin Name: Fail2ban helper
+Description: Pushes failed logins to webserver error log for Fail2Ban use
+Version: 1.0
+Author: zEvilz
+Author URI: https://github.com/zevilz/zevilz-f2b-helper
+License: MIT
+ */
+
+add_action( 'wp_login_failed', function( $username ) {
+	error_log( 'WP LOGIN FAILED for username: ' . $username );
+} );
